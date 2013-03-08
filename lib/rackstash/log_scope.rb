e57ebbb9d90146eec9751e7d871c872feda5ae7e
Holger Just <holger@plan.io>
2013-03-08 18:23:16 +0100

    Capture exception details in buffer fields

--- lib/rackstash/log_scope.rb
+++ lib/rackstash/log_scope.rb
@@ -8,6 +8,17 @@ module Rackstash
       def with_log_buffer(&block)
          push_buffer
          yield
+      rescue Exception => exception
+        # Add some details about an exception to the logs
+        # This won't catch errors in Rails requests as they are catched by
+        # the ActionController::Failsafe middleware before our middleware.
+        fields = {
+          :error => exception.class.name,
+          :error_message => exception.message,
+          :error_trace => exception.backtrace.join("\n")
+        }
+        Rackstash.logger.fields.reverse_merge!(fields) if Rackstash.logger.fields
+        raise
       ensure
         flush_and_pop_buffer
       end
